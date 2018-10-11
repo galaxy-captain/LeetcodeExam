90be3d1e7f624543554524521436bd53081db659
goskkyer@gmail.com <a761349852>
2018-10-11 23:09:25 +0800

    Q4,Q5

--- src/me/exam/leetcode/Q5.java
+++ src/me/exam/leetcode/Q5.java
@@ -8,17 +8,17 @@ public class Q5 implements AbsQuestion {
 
     @Override
     public void test() {
-        String result = longestPalindrome("babad");
+        String result = longestPalindrome2("babad");
         System.out.println(result);
     }
 
-    public String longestPalindrome(String s) {
+    public String longestPalindrome2(String s) {
 
-        if (s == null) return null;
+        if (s == null || s.length() == 0) return "";
 
-        if (s.length() == 1) return s;
-
-        String maxPalindromicStr = "";
+        int length = 0;
+        int start = 0;
+        int end = 0;
 
         char[] cs = s.toCharArray();
 
@@ -31,8 +31,10 @@ public class Q5 implements AbsQuestion {
 
                 if (cs[a] == cs[b]) {
 
-                    if (b - a + 1 >= maxPalindromicStr.length()) {
-                        maxPalindromicStr = new String(Arrays.copyOfRange(cs, a, b + 1));
+                    if (b - a + 1 >= length) {
+                        start = a;
+                        end = b + 1;
+                        length = b - a + 1;
                     }
 
                     a--;
@@ -43,34 +45,101 @@ public class Q5 implements AbsQuestion {
 
             }
 
-        }
+            if (i == cs.length - 1) continue;
 
-        for (int i = 0; i < cs.length - 1; i++) {
+            if (cs[i] == cs[i + 1]) {
+                int a2 = i;
+                int b2 = i + 1;
 
-            if (cs[i] != cs[i + 1]) continue;
+                while (a2 >= 0 && b2 < cs.length) {
 
-            int a = i;
-            int b = i + 1;
+                    if (cs[a2] == cs[b2]) {
 
-            while (a >= 0 && b < cs.length) {
+                        if (b2 - a2 + 1 >= length) {
+                            start = a2;
+                            end = b2 + 1;
+                            length = b2 - a2 + 1;
+                        }
 
-                if (cs[a] == cs[b]) {
-
-                    if (b - a + 1 >= maxPalindromicStr.length()) {
-                        maxPalindromicStr = new String(Arrays.copyOfRange(cs, a, b + 1));
+                        a2--;
+                        b2++;
+                    } else {
+                        break;
                     }
 
-                    a--;
-                    b++;
-                } else {
-                    break;
                 }
-
             }
 
         }
 
-        return maxPalindromicStr;
+        return new String(Arrays.copyOfRange(cs, start, end));
     }
+
+
+
+        public String longestPalindrome(String s) {
+
+            if (s == null || s.length() == 0) return "";
+
+            int length = 0;
+            int start = 0;
+            int end = 0;
+
+            char[] cs = s.toCharArray();
+
+            for (int i = 0; i < cs.length; i++) {
+
+                int a = i;
+                int b = i;
+
+                while (a >= 0 && b < cs.length) {
+
+                    if (cs[a] == cs[b]) {
+
+                        if (b - a + 1 >= length) {
+                            start = a;
+                            end = b + 1;
+                            length = b - a + 1;
+                        }
+
+                        a--;
+                        b++;
+                    } else {
+                        break;
+                    }
+
+                }
+
+            }
+
+            for (int i = 0; i < cs.length - 1; i++) {
+
+                if (cs[i] != cs[i + 1]) continue;
+
+                int a = i;
+                int b = i + 1;
+
+                while (a >= 0 && b < cs.length) {
+
+                    if (cs[a] == cs[b]) {
+
+                        if (b - a + 1 >= length) {
+                            start = a;
+                            end = b + 1;
+                            length = b - a + 1;
+                        }
+
+                        a--;
+                        b++;
+                    } else {
+                        break;
+                    }
+
+                }
+
+            }
+
+            return new String(Arrays.copyOfRange(cs, start, end));
+        }
 
 }
