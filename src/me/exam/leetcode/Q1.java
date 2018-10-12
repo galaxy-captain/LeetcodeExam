8eaf6863a9cf2cf1cab433c3655d23a1cc2bef76
goskkyer@gmail.com <a761349852>
2018-10-12 23:53:44 +0800

    Q1

--- src/me/exam/leetcode/Q1.java
+++ src/me/exam/leetcode/Q1.java
@@ -3,16 +3,18 @@ package me.exam.leetcode;
 import me.exam.leetcode.others.AbsQuestion;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- *
+ * <p>
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Given nums = [2, 7, 11, 15], target = 9,
- *
+ * <p>
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  */
@@ -21,19 +23,19 @@ public class Q1 implements AbsQuestion {
     @Override
     public void test() {
         Q1 q1 = new Q1();
-        int[] result = q1.twoSum(new int[]{2, 7, 11, 15},9);
+        int[] result = q1.twoSum(new int[]{2, 7, 11, 15}, 9);
         System.out.println(Arrays.toString(result));
     }
 
     private int[] twoSum(int[] nums, int target) {
 
-        for(int i = 0;i<nums.length;i++){
+        Map<Integer, Integer> map = new HashMap<>();
 
-            for(int j = i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target)
-                    return new int[]{i,j};
-            }
-
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff))
+                return new int[]{map.get(diff), i};
+            map.put(nums[i], i);
         }
 
         return null;
